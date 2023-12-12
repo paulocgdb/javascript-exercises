2f45abd86e241cca81130673ff2b9426250f4546
Paulo Cesar <zpeusac@ericsson.com>
2023-12-11 22:55:25 -0300

    practice - Simple array functions

---
+++ arrow-functions/e-1.js
@@ -0,0 +1,23 @@
+function sum (a, b) {
+    return a + b;
+}
+
+let sumArrow = (a, b) => a + b;
+console.log(sum(1, 2));
+console.log(sumArrow(1, 2));
+console.log("\n");
+
+function isPositiveNumber (number) {
+    return number > 0;
+}
+let isPositiveNumberArrow = number => number > 0;
+console.log(isPositiveNumber(10));
+console.log(isPositiveNumberArrow(10));
+console.log("\n");
+
+function subtract (a, b) {
+    return a - b;
+}
+let substractArrow = (a, b) => a - b;
+console.log(subtract(10, 5));
+console.log(substractArrow(10, 5));
