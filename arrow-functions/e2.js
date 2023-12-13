132d92caa88ad4cabcff81af1ac7190774251115
Paulo Cesar <paulocesarti01@gmail.com>
2023-12-12 23:53:21 -0300

    practice - FindIdex method

--- arrow-functions/e2.js
+++ arrow-functions/e2.js
@@ -1,8 +1,12 @@
-// Filter method
+// Filter and findIndex methods
+
+let array = [1, 2, 3, 4, 5];
 
 const pairArray = array => array.filter(element => element % 2 === 0);
-let array = [1, 2, 3, 4, 5];
 console.log(pairArray(array));
 
 const oddArray = array => array.filter(element => element % 2 !== 0);
 console.log(oddArray(array));
+
+const findArrow = array => array.findIndex(element => element === 1);
+console.log(findArrow(array));
