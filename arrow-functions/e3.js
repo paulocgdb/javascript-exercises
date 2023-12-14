bd24f6c547ea2e44b6e342341221490d9a9076a1
Paulo Cesar <paulocesarti01@gmail.com>
2023-12-13 23:43:31 -0300

    practice - Simple Objects and map function

--- arrow-functions/e3.js
+++ arrow-functions/e3.js
@@ -4,3 +4,18 @@ const mapArrow = array => array.map(element => element * 2);
 let array = [1, 2, 3, 5, 6];
 
 console.log(mapArrow(array));
+
+let personOne = {
+    name: 'Paulo',
+    age: 29
+}
+
+let personTwo = {
+    name: 'New Paulo',
+    age: 30
+}
+
+const mapAge = array => array.map(element => element.age);
+let personList = [personOne, personTwo];
+
+console.log(mapAge(personList));
