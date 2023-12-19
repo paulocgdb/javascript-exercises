0a457ab8581d1e06833fab4e154945e800e78310
Paulo Cesar <paulocesarti01@gmail.com>
2023-12-18 23:55:29 -0300

    ajuste

--- arrow-functions/e2.js
+++ arrow-functions/e2.js
@@ -5,6 +5,7 @@ let array = [1, 2, 3, 4, 5];
 const pairArray = array => array.filter(element => element % 2 === 0);
 console.log(pairArray(array));
 
+
 const oddArray = array => array.filter(element => element % 2 !== 0);
 console.log(oddArray(array));
 
