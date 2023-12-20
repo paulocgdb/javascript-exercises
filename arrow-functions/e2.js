3300346b1b492e44bf34efb129cd7f22b55911d6
Paulo Cesar <paulocesarti01@gmail.com>
2023-12-19 23:27:07 -0300

    duplicate

--- arrow-functions/e2.js
+++ arrow-functions/e2.js
@@ -5,9 +5,11 @@ let array = [1, 2, 3, 4, 5];
 const pairArray = array => array.filter(element => element % 2 === 0);
 console.log(pairArray(array));
 
-
 const oddArray = array => array.filter(element => element % 2 !== 0);
 console.log(oddArray(array));
 
 const findArrow = array => array.findIndex(element => element === 1);
 console.log(findArrow(array));
+
+const duplicateArrow = array => array.map(element => element * 2);
+console.log(duplicateArrow(array));
