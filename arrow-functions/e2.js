7a6589b3b64d1885b480b298521f026d5803277b
Paulo César <paulocesarti01@gmail.com>
2024-09-04 00:10:04 -0300

    Update e2.js

--- arrow-functions/e2.js
+++ arrow-functions/e2.js
@@ -1,6 +1,6 @@
 // Filter and findIndex methods
 
-let array = [1, 2, 3, 4, 5, 6, 7];
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 8];
 
 const pairArray = array => array.filter(element => element % 2 === 0);
 console.log(pairArray(array));
